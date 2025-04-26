1558cb1c8305ecf36278d8103e740eb8ad725c22
Rishav <kumarrishav28@gmail.com>
2025-04-26 23:29:26 +0530

    4/26 - commit

---
+++ order-service/src/main/java/com/fabrikka/order_service/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.fabrikka.order_service.controller;
+
+import com.fabrikka.order_service.dto.CreateOrderRequest;
+import com.fabrikka.order_service.dto.OrderResponse;
+import com.fabrikka.order_service.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/create")
+    public ResponseEntity<OrderResponse> createOrder(@RequestBody CreateOrderRequest request) {
+        OrderResponse response = orderService.createOrder(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<OrderResponse>> getUserOrders(@PathVariable Long userId) {
+        List<OrderResponse> orders = orderService.getOrdersByUserId(userId);
+        return ResponseEntity.ok(orders);
+    }
+}
