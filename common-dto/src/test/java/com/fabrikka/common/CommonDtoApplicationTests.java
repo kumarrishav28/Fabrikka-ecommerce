e30e7cb02ae3d13c87ea664ff91e3fd3d00134d3
Rishav <kumarrishav28@gmail.com>
2025-05-01 19:09:24 +0530

    01/05 - commit

---
+++ common-dto/src/test/java/com/fabrikka/common/CommonDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrikka.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
